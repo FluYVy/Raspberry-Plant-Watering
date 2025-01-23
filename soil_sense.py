54209517c9866098743c911711daa9b790db5fad
cami0705 <camillo.bechtold@student.htl-rankweil.at>
2025-01-23 12:21:38 +0100

    edited soil_sense.py

--- soil_sense.py
+++ soil_sense.py
@@ -6,16 +6,24 @@ from adafruit_seesaw.seesaw import Seesaw
 i2c_bus = busio.I2C(board.SCL, board.SDA)
 ss = Seesaw(i2c_bus, addr=0x36)
 
-while True:
-    # read moisture level through capacitive touch pad
+def read_moisture():
+    # returns calue between 200 (very dry) and 2000 (very wet)
     touch = ss.moisture_read()
-    # read temperature from the temperature sensor
+    return touch
+
+def read_temp():
     temp = ss.get_temp()
-    print("temp: " + str(temp) + " moisture: " + str(touch))
-    if touch < 400:
+    return temp
+
+while True:
+    moisture = read_moisture()
+    temp = read_temp()
+
+    print("temp: " + str(temp) + " moisture: " + str(moisture))
+    if moisture < 400:
         print("Aaaaaah, gib mir Wasser!!!")
-    if touch > 400 and touch < 1000: 
+    if moisture > 400 and moisture < 1000: 
         print("Angenehm!!!")
-    elif touch > 1000:
+    elif moisture > 1000:
         print("Hiilfe, ich ertrinke!!!")
     time.sleep(1)
